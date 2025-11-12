9d4c4bc0128f5f62cf36f33030188040a4cbcb3f
Bastien PF <bastou2731.pf@gmail.com>
2025-11-12 15:22:53 +0100

    qa: added eslint basic rules

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: { ...globals.node, ...globals.jest },
+    },
+  },
+  {
+    files: ["tests/**/*.js"],
+    languageOptions: {
+      globals: { ...globals.node, ...globals.jest },
+    },
+  },
+];
